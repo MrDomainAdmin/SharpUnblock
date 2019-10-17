e7f3fafb3b5332a1265d4ae0b41d3e184d2ab5c8
MrDomainAdmin <19913511+MrDomainAdmin@users.noreply.github.com>
2019-10-17 18:09:41 -0400

    Version 1 Release

---
+++ SharpUnblock/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpUnblock
+{
+    class Program
+    {
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteFile(string name);
+        static void Main(string[] args)
+        {
+            switch (args.Length)
+            {
+                case 1:
+                    Console.WriteLine("Unblocking " + args[0]);
+                    DeleteFile(args[0] + ":Zone.Identifier");
+                    break;
+                default:
+                    Console.WriteLine("Please enter the full path of a file to unblock");
+                    break;
+            }
+        }
+    }
+}
